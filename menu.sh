b47acd73b0fef20e49b561436db7aca1826fad8f
khairul <88110188+alifvivo@users.noreply.github.com>
2022-03-14 01:50:25 +0800

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -63,9 +63,9 @@ echo -e "$RED ░░░██║░░░██╔══██╗██║██
 echo -e "$RED ░░░██║░░░██║░░██║██║╚█████╔╝██║░╚██╗██████╔╝ $NC"
 echo -e "$RED ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░╚════╝░╚═╝░░╚═╝╚═════╝░ $NC"
 echo -e "$green Premium Script"$NC
-echo -e "${ORANGE}════════════════════════════════════════════════════════════${NC}"
+echo -e "${green}════════════════════════════════════════════════════════════${NC}"
 echo -e "${BGBLUE}                      SERVER INFORMATION                    ${NC}"
-echo -e "${ORANGE}════════════════════════════════════════════════════════════${NC}"
+echo -e "${green}════════════════════════════════════════════════════════════${NC}"
 echo -e "$green CPU Model            :$CYAN $cname"$NC
 echo -e "$green CPU Frequency        :$CYAN $freq MHz"$NC
 echo -e "$green Number Of Cores      :$CYAN $cores"$NC
@@ -88,39 +88,23 @@ echo -e "$green Client Name          :$CYAN $name${NC}"
 echo -e "$green Expired Date         :$CYAN $exp${NC}"
 echo -e "$green Provided By          :$CYAN @anakjati567"$NC
 echo -e "$green Script Version       :$CYAN $Sver"
-echo -e "${ORANGE}════════════════════════════════════════════════════════════${NC}"
+echo -e "${green}════════════════════════════════════════════════════════════${NC}"
 echo -e "${BGBLUE}                     [ MAIN MENU ]                          ${NC}"
-echo -e "${ORANGE}════════════════════════════════════════════════════════════${NC}"
-echo -e "$green (•1) $NC PANEL SSH & OPENVPN"
-echo -e "$green (•2) $NC PANEL WIREGUARDS"
-echo -e "$green (•3) $NC PANEL L2TP "
-echo -e "$green (•4) $NC PANEL PPTP "
-echo -e "$green (•5) $NC PANEL SSTP "
-echo -e "$green (•6) $NC PANEL SSR "
-echo -e "$green (•7) $NC PANEL SS "
-echo -e "$green (•8) $NC PANEL VMESS "
-echo -e "$green (•9) $NC PANEL VLESS "
-echo -e "$green (10) $NC PANEL XRAY "
-echo -e "$green (11) $NC PANEL TROJAN "
-echo -e "${ORANGE}════════════════════════════════════════════════════════════${NC}"
-echo -e "${BGBLUE}                     [ SISTEM MENU ]                        ${NC}"
-echo -e "${ORANGE}════════════════════════════════════════════════════════════${NC}"
-echo -e "$green (•12) $NC ADD NEW SUBDOMAIN     $green (•20) $NC Limit Bandwith "
-echo -e "$green (•13) $NC RENEW CERTIFICATE     $green (•21) $NC Check Usage "
-echo -e "$green (•14) $NC CHANGE PORT           $green (•22) $NC Reboot VPS "
-echo -e "$green (•15) $NC AUTOBACKUP DATA       $green (•23) $NC Speedtest VPS "
-echo -e "$green (•16) $NC BACKUP DATA VPS       $green (•24) $NC Update Script "
-echo -e "$green (•17) $NC Restore Data Vps      $green (•25) $NC Displaying System "
-echo -e "$green (•18) $NC WEBMIN MENU           $green (•26) $NC Info Script Auto "
-echo -e "$green (•19) $NC UPDATE TO KERNEL      $green (•27) $NC Show System Status "
-echo -e "${ORANGE}════════════════════════════════════════════════════════════${NC}"
+echo -e "${green}════════════════════════════════════════════════════════════${NC}"
+echo -e "$green (•1) $NC PANEL SSH & OPENVPN         $green (•7) $NC SYSTEM MENU "
+echo -e "$green (•2) $NC PANEL WIREGUARDS            $green (•8) $NC CHANGE PORT VPN "
+echo -e "$green (•3) $NC PANEL VMESS & VLESS         $green (•9) $NC Info Script Auto "
+echo -e "$green (•4) $NC PANEL TROJAN                $green (10) $NC Check Usage "
+echo -e "$green (•5) $NC PANEL SS & SSR              $green (11) $NC REBOOT VPS "
+echo -e "$green (•6) $NC PANEL L2TP, PPTP, SSTP      $green (12) $NC CHECK RUNNING SC "
+echo -e "${green}════════════════════════════════════════════════════════════${NC}"
 echo -e " Premium VPS by @anakjati567"
 echo -e " Thank you for using script by PAKYAVPN"
-echo -e "${ORANGE}════════════════════════════════════════════════════════════${NC}"
+echo -e "${green}════════════════════════════════════════════════════════════${NC}"
 echo -e   ""
 echo -e "[Ctrl + C] For exit from main menu"
 echo -e   ""
-read -p "Select From Options [1-27 or x] :  " menu
+read -p "Select From Options [1-12 or x] :  " menu
 echo -e ""
 case $menu in
 1)
@@ -130,78 +114,33 @@ menu-ssh
 menu-wg
 ;;
 3)
-menu-l2tp
+menu-v2ray
 ;;
 4)
-menu-pptp
+menu-trojan
 ;;
 5)
-menu-sstp
-;;
-6)
 menu-ssr
 ;;
+6)
+menu-pptp
+;;
 7)
-menu-ss 
+system-menu
 ;;
 8)
-menu-vmess
-;;
-9)
-menu-vless 
-;;
-10)
-menu-xray 
-;;
-11)
-menu-trojan 
-;;
-12)
-add-host
-;;
-13)
-certv2ray
-;;
-14)
 change-port
 ;;
-15)
-autobackup
-;;
-16)
-backup
-;;
-17)
-restore
-;;
-18)
-wbmn
-;;
-19)
-kernel-updt
-;;
-20)
-limit-speed
-;;
-21)
-ram
-;;
-22)
-reboot
-;;
-23)
-speedtest-cli
-;;
-24)
-update
-;;
-25)
-info
-;;
-26)
+9)
 about
 ;;
-27)
+10)
+ram
+;;
+11)
+reboot
+;;
+12)
 running
 ;;
 x)
