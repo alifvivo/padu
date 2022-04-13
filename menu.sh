75c85442d8fc1d3fffc69b236629bb483f965387
khairul <88110188+alifvivo@users.noreply.github.com>
2022-04-14 01:39:50 +0800

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -1,108 +1,41 @@
 #!/bin/bash
-Lyellow='\e[1;93m'
-green='\e[32m'
+Lred='\e[1;91m'
+LGREEN='\e[92m'
+Lyellow='\e[93m'
+GREEN='\e[32m'
 RED='\033[0;31m'
 NC='\033[0m'
 BGBLUE='\e[1;44m'
+BGWHITE='\e[0;47;30m'
 ORANGE='\033[0;33m'
 BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 NC='\033[0;37m'
-# ==========================================
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
-
-BURIQ () {
-    curl -sS https://raw.githubusercontent.com/alifvivo/ip/main/access > /root/tmp
-    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-    for user in "${data[@]}"
-    do
-    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-    d1=(`date -d "$exp" +%s`)
-    d2=(`date -d "$biji" +%s`)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-    echo $user > /etc/.$user.ini
-    else
-    rm -f /etc/.$user.ini > /dev/null 2>&1
-    fi
-    done
-    rm -f /root/tmp
-}
-
-MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/alifvivo/ip/main/access | grep $MYIP | awk '{print $2}')
-echo $Name > /usr/local/etc/.$Name.ini
-CekOne=$(cat /usr/local/etc/.$Name.ini)
-
-Bloman () {
-if [ -f "/etc/.$Name.ini" ]; then
-CekTwo=$(cat /etc/.$Name.ini)
-    if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
-    fi
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$(curl -sS https://raw.githubusercontent.com/alifvivo/ip/main/access | awk '{print $4}' | grep $MYIP)
+if [[ $MYIP = $IZIN ]]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
-res="Permission Accepted..."
-fi
-}
-
-PERMISSION () {
-    MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/alifvivo/ip/main/access | awk '{print $4}' | grep $MYIP)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
-    BURIQ
-}
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-BOLD='\e[1m'
-BLINK='\e[5m'
-END='\e[0m'
-RED_B='\e[1;91m'
-GREEN_B='\e[1;92m'
-YELLOW_B='\e[1;93m'
-BLUE_B='\e[1;94m'
-PURPLE_B='\e[1;95m'
-CYAN_B='\e[1;96m'
-WHITE_B='\e[1;97m'
-RESET='\e[0m'
-red() { echo -e "${RED_B}${1}${RESET}"; }
-green() { echo -e "${GREEN_B}${1}${RESET}"; }
-yellow() { echo -e "${YELLOW_B}${1}${RESET}"; }
-blue() { echo -e "${BLUE_B}${1}${RESET}"; }
-purple() { echo -e "${PURPLE_B}${1}${RESET}"; }
-cyan() { echo -e "${CYAN_B}${1}${RESET}"; }
-white() { echo -e "${WHITE_B}${1}${RESET}"; }
-PERMISSION
-
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
-green "Tahniah! Anda Dibenarkan menggunakan PAKYAVPN..."
-sleep 1
-else
-clear
-blue        "                   ...              "             
-blue        "Anda Tidak Dibenarkan Menggunakan Script ini!    " 
-blue        "                   ...                     "      
-blue    "Hubungi Saya di Telegram Untuk Mendapatkan Kebenaran!  "   
-blue    "                  t.me/anakjati567     "
-exit 0
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${Lyellow}Please Contact Admin!!"
 fi
 clear
-echo -e ""
+# VPS Information
+Checkstart1=$(ip route | grep default | cut -d ' ' -f 3 | head -n 1);
+if [[ $Checkstart1 == "venet0" ]]; then
+    clear
+	  lan_net="venet0"
+    typevps="OpenVZ"
+    sleep 1
+else
+    clear
+		lan_net="eth0"
+    typevps="premium"
+    sleep 1
+fi
+clear
 # DNS Patch
 tipeos2=$(uname -m)
 # OS Uptime
@@ -136,7 +69,6 @@ WKT=$(curl -s ipinfo.io/timezone )
 IPVPS=$(curl -s ipinfo.io/ip )
 domain=$(cat /etc/v2ray/domain)
 Sver=$(cat /home/version)
-tele=$(cat /home/contact)
 	cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
 	cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
 	freq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
@@ -146,51 +78,62 @@ tele=$(cat /home/contact)
 	swap=$( free -m | awk 'NR==4 {print $2}' )
 name=$(curl -sS https://raw.githubusercontent.com/alifvivo/ip/main/access | grep $IPVPS | awk '{print $2}')
 exp=$(curl -sS https://raw.githubusercontent.com/alifvivo/ip/main/access | grep $IPVPS | awk '{print $3}')
+modifyTime1=$(date +%s -d "${exp}")
+currentTime=$(date +%s)
+stampDiff=$(expr ${currentTime} - ${modifyTime1})
+days=$(expr ${stampDiff} / 86400)
+remainingDays=$(expr 90 - ${days})
+remain=${remainingDays}
+if [[ ${remainingDays} -le 0 ]]; then
+	remain="expired"
+fi
 clear
-figlet SQUIDWARD | lolcat
-echo -e "$green Premium Script"$NC
-echo -e "${green}════════════════════════════════════════════════════════════${NC}"
-echo -e "${BGBLUE}                      SERVER INFORMATION                    ${NC}"
-echo -e "${green}════════════════════════════════════════════════════════════${NC}"
-echo -e "$green CPU Model            :$CYAN $cname"$NC
-echo -e "$green CPU Frequency        :$CYAN $freq MHz"$NC
-echo -e "$green Number Of Cores      :$CYAN $cores"$NC
-echo -e "$green CPU Usage            :$CYAN $cpu_usage"$NC
-echo -e "$green Operating System     :$CYAN "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`$NC
-echo -e "$green Kernel               :$CYAN `uname -r`"$NC
-echo -e "$green Bash Ver             :$CYAN $versibash"$NC
-echo -e "$green Total Amount Of RAM  :$CYAN $tram MB"$NC
-echo -e "$green Used RAM             :$CYAN $uram MB"$NC
-echo -e "$green Free RAM             :$CYAN $fram MB"$NC
-echo -e "$green System Uptime        :$CYAN $uptime $DF( From VPS Booting )"$NC
-echo -e "$green Download             :$CYAN $downloadsize GB ( From Startup / VPS Booting )"$NC
-echo -e "$green Upload               :$CYAN $uploadsize GB ( From Startup / VPS Booting )"$NC
-echo -e "$green ISP NAME             :$CYAN $ISP"$NC
-echo -e "$green IP VPS               :$CYAN $IPVPS"$NC
-echo -e "$green DOMAIN               :$CYAN $domain"$NC
-echo -e "$green City                 :$CYAN $CITY"$NC
-echo -e "$green SERVER               :$CYAN $ISP"$NC
-echo -e "$green Client Name          :$CYAN $name${NC}"
-echo -e "$green Expired Date         :$CYAN $exp${NC}"
-echo -e "$green Provided By          :$CYAN @SandakanVPNTrickster"$NC
-echo -e "$green Script Version       :$CYAN $Sver"
-echo -e "${green}════════════════════════════════════════════════════════════${NC}"
-echo -e "${BGBLUE}                     [ MAIN MENU ]                          ${NC}"
-echo -e "${green}════════════════════════════════════════════════════════════${NC}"
-echo -e "$PURPLE (•1) $NC PANEL SSH & OPENVPN         $green (•7) $NC SYSTEM MENU "
-echo -e "$PURPLE (•2) $NC PANEL WIREGUARDS            $green (•8) $NC CHANGE PORT VPN "
-echo -e "$PURPLE (•3) $NC PANEL VMESS & VLESS         $green (•9) $NC Info Script Auto "
-echo -e "$PURPLE (•4) $NC PANEL TROJAN                $green (10) $NC Check Usage "
-echo -e "$PURPLE (•5) $NC PANEL SS & SSR              $green (11) $NC REBOOT VPS "
-echo -e "$PURPLE (•6) $NC PANEL L2TP, PPTP, SSTP      $green (12) $NC CHECK RUNNING SC "
-echo -e "${green}════════════════════════════════════════════════════════════${NC}"
-echo -e "$Lyellow Premium VPS by @SandakanVPNTrickster"$NC
-echo -e "$Lyellow Thank you for using script by PAKYAVPN"$NC
-echo -e "${green}════════════════════════════════════════════════════════════${NC}"
+echo -e ""
+echo -e "$RED ██████╗ ██████╗ ██╗   ██╗ ██████╗ ██╗   ██╗██████╗ ███╗   ██╗$NC"
+echo -e "$RED ██╔══██╗██╔══██╗██║   ██║██╔════╝ ██║   ██║██╔══██╗████╗  ██║$NC"
+echo -e "$RED ██║  ██║██████╔╝██║   ██║██║  ███╗██║   ██║██████╔╝██╔██╗ ██║$NC"
+echo -e "$RED ██║  ██║██╔══██╗██║   ██║██║   ██║╚██╗ ██╔╝██╔═══╝ ██║╚██╗██║$NC"
+echo -e "$RED ██████╔╝██║  ██║╚██████╔╝╚██████╔╝ ╚████╔╝ ██║     ██║ ╚████║$NC"
+echo -e "$RED ╚═════╝ ╚═╝  ╚═╝ ╚═════╝  ╚═════╝   ╚═══╝  ╚═╝     ╚═╝  ╚═══╝$NC"
+echo -e "$Lyellow Premium Script"$NC
+echo -e "${Lred}════════════════════════════════════════════════════════════${NC}"
+echo -e "${BGWHITE}                      SERVER INFORMATION                    ${NC}"
+echo -e "${Lred}════════════════════════════════════════════════════════════${NC}"
+echo -e " $GREEN VPS Type             : $typevps"$NC
+echo -e " $GREEN CPU Model            :$cname"$NC
+echo -e " $GREEN CPU Frequency        :$freq MHz"$NC
+echo -e " $GREEN Number Of Cores      : $cores"$NC
+echo -e " $GREEN CPU Usage            : $cpu_usage"$NC
+echo -e " $GREEN Operating System     : "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`$NC
+echo -e " $GREEN Kernel               : `uname -r`"$NC
+echo -e " $GREEN Bash Ver             : $versibash"$NC
+echo -e " $GREEN ISP NAME             : $ISP"$NC
+echo -e " $GREEN IP VPS               : $IPVPS"$NC
+echo -e " $GREEN DOMAIN               : $domain"$NC
+echo -e " $GREEN City                 : $CITY"$NC
+echo -e " $GREEN SERVER               : $ISP"$NC
+echo -e " $GREEN Script Version       : $Sver"$NC
+echo -e " $GREEN Client Name          : $name"$NC
+echo -e " $GREEN Expired On           : $exp"$NC
+echo -e " $GREEN Expired In           : $remain Days"$NC
+echo -e " $GREEN Provided By          : $@SandakanVPNTrickster"$NC
+echo -e "${Lred}════════════════════════════════════════════════════════════${NC}"
+echo -e "${BGWHITE}                     [ MAIN MENU ]                          ${NC}"
+echo -e "${Lred}════════════════════════════════════════════════════════════${NC}"
+echo -e " $PURPLE (•1) $NC $Lyellow PANEL SSH & OPENVPN       $PURPLE (•7) $NC $Lyellow SYSTEM MENU $NC"
+echo -e " $PURPLE (•2) $NC $Lyellow PANEL WIREGUARDS          $PURPLE (•8) $NC $Lyellow CHANGE PORT VPN $NC"
+echo -e " $PURPLE (•3) $NC $Lyellow PANEL VMESS & VLESS       $PURPLE (•9) $NC $Lyellow Info Script Auto $NC"
+echo -e " $PURPLE (•4) $NC $Lyellow PANEL TROJAN              $PURPLE (10) $NC $Lyellow Check Usage $NC"
+echo -e " $PURPLE (•5) $NC $Lyellow PANEL SS & SSR            $PURPLE (11) $NC $Lyellow REBOOT VPS $NC"
+echo -e " $PURPLE (•6) $NC $Lyellow PANEL L2TP, PPTP, SSTP    $PURPLE (12) $NC $Lyellow CHECK RUNNING SC $NC"
+echo -e "${Lred}════════════════════════════════════════════════════════════${NC}"
+echo -e " $Lyellow Premium VPS by @SandakanVPNTrickster"$NC
+echo -e " $Lyellow Thank you for using script by PAKYAVPN"$NC
+echo -e "${Lred}════════════════════════════════════════════════════════════${NC}"
 echo -e   ""
-echo -e "[Ctrl + C] For exit from main menu"
+echo -e "$Lyellow    [Ctrl + C] For exit from main menu"
 echo -e   ""
-read -p "Select From Options [1-12 or x] :  " menu
+read -p "    Select From Options [1-12 or x: " menu
 echo -e ""
 case $menu in
 1)
