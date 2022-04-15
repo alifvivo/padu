337496ae384f79fa2dfa47f59beaa438e00d1fbb
khairul <88110188+alifvivo@users.noreply.github.com>
2022-04-16 02:34:58 +0800

    Add files via upload

--- install-xray.sh
+++ install-xray.sh
@@ -55,8 +55,7 @@ EOF
 ssl_path_crt="/etc/v2ray/v2ray.crt"
 ssl_path_key="/etc/v2ray/v2ray.key"
 Port1=6769
-Port2=6769
-Port3=8000
+Port2=8000
 uuid=$(cat /proc/sys/kernel/random/uuid)
 
 # // Vless Splice
@@ -126,7 +125,7 @@ cat > /etc/xray-mini/vless-splice.json << END
 {
     "inbounds": [
         {
-            "port": ${Port2},
+            "port": 6769,
             "protocol": "vless",
             "settings": {
                 "clients": [
@@ -172,7 +171,7 @@ cat > /etc/xray-mini/xtrojan.json << END
 {
     "inbounds": [
         {
-            "port": ${Port3},
+            "port": ${Port2},
             "protocol": "trojan",
             "settings": {
                 "clients": [
